77c5872d82bc7a707ef2524e78145bd2754b5fd9
Blackbluue <miller_raymone@hotmail.com>
2020-03-31 19:35:10 -0400

    Python Exercise 1 Snackin

---
+++ snackin.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+
+def main():
+    favorite_snack = "Lemon Icebox Pie"
+    a = "I could eat"
+    b = 100
+    c = favorite_snack
+    print("My favorite snack is %s" % favorite_snack)
+    print(a, b, c)
+    for i in range(77):
+        print("My favorite snack is %s" % favorite_snack)
+
+
+if __name__ == "__main__":
+    main()
